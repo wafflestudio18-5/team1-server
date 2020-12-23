51e78e4b89abeb9ba04891ddee1a1a668241221b
Jonghyun Choe <andy.choe729@gmail.com>
2020-12-23 19:59:40 +0900

    Removed search functionality with username
    - Error occurs when userFirstName, userLastName is given in search_fields

--- clone_linkedin/post/views.py
+++ clone_linkedin/post/views.py
@@ -13,7 +13,7 @@ from post.models import Post, Comment
 class PostViewSet(viewsets.GenericViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    search_fields = ['title', 'content', 'userFirstName', 'userLastName']
+    search_fields = ['title', 'content']
     filter_backends = (filters.SearchFilter, )
 
     def get_serializer_class(self):

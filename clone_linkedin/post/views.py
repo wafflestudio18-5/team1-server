d59020faf60a17ff789d9e6e7b417f4190ab2a8e
Jonghyun Choe <52521749+201432420@users.noreply.github.com>
2020-12-26 13:23:02 +0900

    Feature/#11 (#16)
    * Created PostReactionSerializer
    
    * Fix Reaction API (GET, POST, DELETE)
    
    * Update serializers.py and views.py
    
    * Update clone_linkedin/post/views.py
    
    Co-authored-by: gina0605 <69783927+gina0605@users.noreply.github.com>
    
    Co-authored-by: gina0605 <69783927+gina0605@users.noreply.github.com>

--- clone_linkedin/post/views.py
+++ clone_linkedin/post/views.py
@@ -2,10 +2,12 @@
 
 from rest_framework import status, viewsets
 # from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
-from post.serializers import PostSerializer, PostDetailSerializer, CommentSerializer
+from post.serializers import PostSerializer, PostDetailSerializer, PostReactionSerializer, CommentSerializer 
 from post.models import Post, Comment
 
 # Create your views here.
@@ -15,7 +17,9 @@ class PostViewSet(viewsets.GenericViewSet):
     serializer_class = PostSerializer
 
     def get_serializer_class(self):
-        if self.action == 'comment':
+        if self.action == 'reaction':
+            return PostReactionSerializer
+        elif self.action == 'comment':
             return CommentSerializer
         elif self.action == 'retrieve':
             return PostDetailSerializer
@@ -56,6 +60,37 @@ class PostViewSet(viewsets.GenericViewSet):
     def destroy(self, request, pk=None):
         self.get_object().delete()
         return Response()
+        
+    # GET, POST, PUT, DELETE /posts/:id/reaction/
+    @action(detail=True, methods=['GET', 'POST', 'PUT', 'DELETE'], url_path='reaction')
+    def reaction(self, request, pk=None):
+        post = self.get_object()
+        data = request.data.copy()
+        if self.request.method == 'GET':
+            reaction = post.postReactions.all()
+            return Response(self.get_serializer(reaction, many=True).data)
+        elif self.request.method == 'POST':
+            user = User.objects.get(id=1)          # Needs to be fixed after login implemented.
+            if self.get_object().postReactions.filter(user=user).exists():
+                return Response({"error": "Reaction already made."}, status=status.HTTP_400_BAD_REQUEST)
+            data['user_id'] = 1
+            data['post_id'] = post.id 
+            serializer = self.get_serializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        elif self.request.method == 'PUT':
+            user = User.objects.get(id=1)          # Needs to be fixed after login implemented.
+            reaction = get_object_or_404(post.postReactions, user=user)
+            serializer = self.get_serializer(reaction, data=data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.update(reaction, serializer.validated_data)
+            return Response(serializer.data)     
+        elif self.request.method == 'DELETE':
+            user = User.objects.get(id=1)       # Needs to be fixed after login implemented.
+            reaction = get_object_or_404(self.get_object().postReactions, user=user)     # Error if multiple reactions by the user exist
+            reaction.delete()
+            return Response()
 
     @action(methods=['post'], detail=True)
     def comment(self, request, pk=None):
